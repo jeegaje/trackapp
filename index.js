db76720c6bda184d20532a5535b984b9336342b0
Angga Jiyan <anggajiyanfajar@gmail.com>
2023-08-16 22:12:47 +0700

    sixth commit

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = 'https://wyntpguqruuohlkekssl.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind5bnRwZ3VxcnV1b2hsa2Vrc3NsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEyODg5MTcsImV4cCI6MjAwNjg2NDkxN30.ifJprJ3AwqX7ogelji5Xbbw-Tv--akgZhOCnxkShrf4'
+const supabase = createClient(supabaseUrl, supabaseKey)
 const app = express();
 
 app.post("/", (req, res) => {
-  res.send(req.body);
+async function getData() {
+  const { data, error } = await supabase
+  .from('transactions')
+  .insert([
+    { transaction_value: req.body},
+  ])
+  .select()
+}
+getData()
+res.send(req.body);
 });
 
 app.listen(5000, () => {
